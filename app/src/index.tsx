65e33d215df3c3a2a9ec0a33a3c7e3ee68aa02b1
Ed Henderson <ed@kingunicorn.co>
2021-06-16 16:01:08 +0100

    Gesta fonts installed & wired up correctly to app #42 (#43)

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './css/reset.css';
+import './css/font-face.css';
 import './css/shame.css';
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
