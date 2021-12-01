53aa5d976dda0bc3a6deb3b462a314f205610409
s4birli <sabirli31@gmail.com>
2021-12-01 18:02:49 +0000

    new xml data integration

--- app/src/components/NavItem.tsx
+++ app/src/components/NavItem.tsx
@@ -21,8 +21,8 @@ export default function NavItem(props: OwnProps) {
   const [selected, setSelected] = useState("");
 
   const handleClick = (href: string) => {
-    setSelectedItem(data.href);
-    window.location.href = href;
+    setSelectedItem(data.id);
+    window.location.href = `#${href}`;
   }
 
   useEffect(() => {
@@ -30,12 +30,12 @@ export default function NavItem(props: OwnProps) {
   },[active]);
 
   return (
-    <li className="h1" key={data.href}>
+    <li className="h1" key={data.id}>
       <div
         className={classnames("collapse-group", { active: active })}
-        onClick={(e) => handleClick(data.href)}
+        onClick={(e) => handleClick(data.id)}
       >
-         {data.content}
+         <div className="menu-text">{data.title}</div>
         {data.children.length > 0 && (
           <div
             className={classnames("collapse-button", { expand: !collapse })}
@@ -46,8 +46,8 @@ export default function NavItem(props: OwnProps) {
       <ul className={classnames({ collapse: collapse })}>
         {data.children.length > 0 &&
           data.children.map((item: any) => (
-            <li className={classnames("h2 subitem", {"item-active": item.href === selected})} key={item.href} onClick={(e) => {handleClick(item.href);setSelected(item.href)} }>
-              {item.content}
+            <li className={classnames("h2 subitem", {"item-active": item.id === selected})} key={item.id} onClick={(e) => {handleClick(item.id);setSelected(item.id)} }>
+              {item.title}
             </li>
           ))}
       </ul>
