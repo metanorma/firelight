507ce8cebd93c70bc2cfa8d29f78d1538f2cdb1e
s4birli <sabirli31@gmail.com>
2021-12-01 16:20:18 +0000

    add the feature for getting sidebar items from xml data

--- app/src/components/NavMenu.tsx
+++ app/src/components/NavMenu.tsx
@@ -1,8 +1,9 @@
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-import { useState } from "react";
+import { useMemo, useEffect, useState } from "react";
 import classnames from "classnames";
 import NavItem from "./NavItem";
+// import axios from 'axios';
 
 import "./NavMenu.css";
 import datas from "../data/sidebar.json";
@@ -10,29 +11,98 @@ import datas from "../data/sidebar.json";
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 interface OwnProps {
-  children: JSX.Element | JSX.Element[];
-  content: JSX.Element | JSX.Element[];
+  xmlData: any;
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-export default function NavIMenu() {
+export default function NavIMenu({ xmlData }: OwnProps) {
   const [selectedItem, setSelectedItem] = useState("");
+  const menuItem = useMemo(() => {
+    const insertSpace = (text: string): any => {
+      const matches: any = text.match(/[a-zA-Z]+/g);
+      const index: number = text.indexOf(matches[0]);
+      return index ? text.substr(0, index) + "  " + text.substr(index) : text;  
+    }
 
+    const getMenuItem = (data: any): any => {
+      const returnData: any = {};
+      returnData.id = data["$"]["id"];
+      returnData.index = data["$"]["displayorder"];
+      returnData.title = insertSpace(
+        typeof data["title"][0] === "string"
+          ? data["title"][0]
+          : data["title"][0]["_"]);
+      if (data['title'][0]['strong'] && data['title'][0]['strong'].length > 0) {
+        returnData.title = `${data['title'][0]['strong'][0]} ${data["title"][0]["_"]} ${data['title'][0]['strong'][1]}`; 
+      }
+      returnData.children = [];
+      if (data?.clause?.length > 1) {
+        data.clause.map((item: any, index: number) => {
+          const childItem: any = {};
+          childItem.id = item["$"]["id"];
+          childItem.title = insertSpace(
+            typeof item["title"][0] === "string"
+              ? item["title"][0]
+              : item["title"][0]["_"]);
+          returnData.children[index] = childItem;
+        });
+      }
+      return returnData;
+    };
+
+    const menuItem: any[] = [];
+    if (xmlData["bsi-standard"]) {
+      //the foreword part for menu item
+      const foreword = getMenuItem(xmlData["bsi-standard"]["preface"][0]["foreword"][0]);
+      menuItem[foreword.index] = foreword;
+      // menuItem.push(foreword);
+      //the introduction part for menu item
+      const introduction = getMenuItem(xmlData["bsi-standard"]["preface"][0]["introduction"][0]);
+      menuItem[introduction.index] = introduction;
+      // menuItem.push(introduction);
+      //the introduction part for menu item
+      const references = getMenuItem(xmlData["bsi-standard"]["bibliography"][0]["references"][0]);
+      menuItem[references.index] = references;
+      // menuItem.push(references);
+      //the terms part for menu item
+      const terms = getMenuItem(xmlData["bsi-standard"]["sections"][0]["terms"][0]);
+      menuItem[terms.index] = terms;
+      // menuItem.push(terms)
+      //the sction part for menu item
+      const sections = xmlData["bsi-standard"]["sections"][0]["clause"];
+      if (sections?.length) {
+        sections.map((sectoin: any) => {
+          const sectionItem = getMenuItem(sectoin);
+          menuItem[sectionItem.index] = sectionItem;
+          // menuItem.push(sectionItem);
+        })
+      }
+      //the sction part for menu item
+      const annex = xmlData["bsi-standard"]["annex"];
+      if (annex?.length) {
+        annex.map((sectoin: any) => {
+          const sectionItem = getMenuItem(sectoin);
+          menuItem[sectionItem.index] = sectionItem;
+          // menuItem.push(sectionItem)
+        })
+      }
+    }
+    return menuItem;
+  }, [xmlData]);
+  console.log(menuItem);
   return (
     <nav>
-      {/* <h1 id="content">
-        <a
-          className="anchorjs-link "
-          aria-label="Anchor"
-          data-anchorjs-icon=""
-          href="#content"
-        ></a>
-        Contents
-      </h1> */}
       <div id="toc">
         <ul>
-          {datas.map(item => <NavItem data={item} active={selectedItem === item.href} setSelectedItem={setSelectedItem}/>)}
+          {menuItem.map((item, index) => (
+            <NavItem
+              key={index}
+              data={item}
+              active={selectedItem === item.id}
+              setSelectedItem={setSelectedItem}
+            />
+          ))}
         </ul>
       </div>
     </nav>
