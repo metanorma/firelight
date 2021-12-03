5504c184609c5da84bc163fc317b63f4ce9204ec
s4birli <sabirli31@gmail.com>
2021-12-03 15:33:20 +0000

    create the strong tag component

---
+++ app/src/components/StrongTag.tsx
@@ -0,0 +1,20 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+import { useMemo } from "react";
+import "./StrongTag.css";
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+interface OwnProps {
+  data: any;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default function StrongTag({ data }: OwnProps) {
+    const renderContent = useMemo(() => {
+        const value = data.childNodes[0].data;
+        return <strong>{value}</strong>;
+      }, [data]);
+
+  return <>{renderContent}</>;
+}
+
