b0f8fd3d0431943188204ff81fdb6253d0bbbdb8
s4birli <sabirli31@gmail.com>
2021-12-03 15:44:41 +0000

    split the container and terminal component by id

--- app/src/components/ContentSection.tsx
+++ app/src/components/ContentSection.tsx
@@ -1,6 +1,7 @@
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 import SectionTitle from "./SectionTitle";
 import SectionP from "./SectionP";
+import SectionOl from "./SectionOl";
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 interface OwnProps {
@@ -10,14 +11,27 @@ interface OwnProps {
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 export default function ContentSection({ xmlData }: OwnProps) {
-  const id = xmlData["$"]["id"] ? xmlData["$"]["id"] : "";
+  const id = xmlData?.$?.id ? xmlData.$.id : "";
 
-  return (
+  return id ? (
     <div className="content-section" id={id}>
       {xmlData?.title?.length && <SectionTitle title={xmlData.title[0]} />}
       {xmlData?.p?.length && <SectionP data={xmlData.p} />}
+      {xmlData?.ol?.length && <SectionOl data={xmlData.ol[0]} />}
       {xmlData?.clause?.length > 0 &&
-        xmlData.clause.map((child: any) => <ContentSection xmlData={child} />)}
+        xmlData.clause.map((child: any, index: number) => (
+          <ContentSection key={index} xmlData={child} />
+        ))}
     </div>
+  ) : (
+    <>
+      {xmlData?.title?.length && <SectionTitle title={xmlData.title[0]} />}
+      {xmlData?.p?.length && <SectionP data={xmlData.p} />}
+      {xmlData?.ol?.length && <SectionOl data={xmlData.ol[0]} />}
+      {xmlData?.clause?.length > 0 &&
+        xmlData.clause.map((child: any, index: number) => (
+          <ContentSection key={index} xmlData={child} />
+        ))}
+    </>
   );
 }
