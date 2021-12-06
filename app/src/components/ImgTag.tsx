916d56b5fd8eb613b441e8737ec59cb0d9300391
s4birli <sabirli31@gmail.com>
2021-12-06 12:38:28 +0000

    create the img tag component

---
+++ app/src/components/ImgTag.tsx
@@ -0,0 +1,24 @@
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+import { useMemo } from "react";
+import "./ImgTag.css";
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+interface OwnProps {
+  data: any;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default function ImgTag({ data }: OwnProps) {
+    const renderContent = useMemo(() => {
+        const attrs: any = {};
+        const attr = data?.attributes;
+        Object.values(attr).map((item: any) => {
+          if (item.name) attrs[item.name] = item.value;
+        });
+        return <img className="img" {...attrs} alt="figure" />;
+      }, [data]);
+
+  return <>{renderContent}</>;
+}
