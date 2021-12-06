b05ee32016fcdc33a1e5b864b79181cb85b92067
s4birli <sabirli31@gmail.com>
2021-12-06 20:09:05 +0000

    update the display node component

--- app/src/components/DisplayNode.tsx
+++ app/src/components/DisplayNode.tsx
@@ -1,9 +1,15 @@
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 import { useMemo } from "react";
-import { getChildsById } from "../utility";
 import LinkTag from "./LinkTag";
 import StrongTag from "./StrongTag";
 import XrefTag from "./XrefTag";
+import NameTag from "./NameTag";
+import ImgTag from "./ImgTag";
+import SectionTitle from "./SectionTitle";
+import SectionP from "./SectionP";
+import SectionNote from "./SectionNote";
+import SectionFigure from "./SectionFigure";
+import SectionOl from "./SectionOl";
 import "./DisplayNode.css";
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -25,6 +31,33 @@ export default function DisplayNode({ data }: OwnProps) {
       }
       if (item.tagName === "xref") {
         return <XrefTag data={item} key={index}/>;
+      }
+      if (item.tagName === "tab") {
+        return <span key={index}>{"    "}</span>;
+      }
+      if (item.tagName === "\n") {
+        return <></>;
+      }
+      if (item.tagName === "name") {
+        return <NameTag data={item} key={index} />
+      }
+      if (item.tagName === "image") {
+        return <ImgTag data={item} key={index} />
+      }
+      if (item.tagName === 'title') {
+        return <SectionTitle title={item} key={index}/>;
+      }
+      if (item.tagName === 'p') {
+        return <SectionP data={item} key={index}/>
+      }
+      if (item.tagName === 'note') {
+        return <SectionNote data={item} key={index} />
+      }
+      if (item.tagName === 'ol') {
+        return <SectionOl data={item} key={index} />
+      }
+      if (item.tagName === 'figure') {
+        return <SectionFigure data={item} key={index} />
       }
     });
   }, [data]);
