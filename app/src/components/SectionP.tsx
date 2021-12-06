fc39e4b5f08a2bb7de761f0598c186e11fc76b64
s4birli <sabirli31@gmail.com>
2021-12-06 12:37:40 +0000

    update the p tag

--- app/src/components/SectionP.tsx
+++ app/src/components/SectionP.tsx
@@ -1,26 +1,24 @@
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-import { render } from "@testing-library/react";
 import { useMemo } from "react";
-import { getChildsById } from "../utility";
 import DisplayNode from "./DisplayNode";
 import "./SectionTitle.css";
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 interface OwnProps {
-  data: any[];
+  data: any;
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 export default function SectionP({ data }: OwnProps) {
   const renderContent = useMemo(() => {
-    return data.map((item: any) => {
-        if (typeof item === 'string') return <p>{item}</p>;
-        const id = item['$']['id'];
-        const node: any = getChildsById(id);
-        return <p id={id} key={id}><DisplayNode data={node.childNodes}/></p>;
-    })
+    const attrs: any[] = data.attributes;
+    const depthRow = Object.values(attrs).find(
+      (attr: any) => attr?.name === "id"
+    );
+    const id = depthRow?.value ? depthRow.value : "";
+    return <div className="p" id={id}><DisplayNode data={data.childNodes} /></div>
   }, [data]);
 
   return <>{renderContent}</>;
