7d5508a7da1dda34a272e5705b209765f412b04d
s4birli <sabirli31@gmail.com>
2021-12-07 12:33:36 +0000

    update the xref tag

--- app/src/components/XrefTag.tsx
+++ app/src/components/XrefTag.tsx
@@ -1,5 +1,6 @@
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 import { useMemo } from "react";
+import DisplayNode from "./DisplayNode";
 import "./XrefTag.css";
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -10,14 +11,22 @@ interface OwnProps {
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 export default function XrefTag({ data }: OwnProps) {
-    const renderContent = useMemo(() => {
-        const attr = data?.attributes;
-        const child = data?.childNodes;
-        const attrs: any = {};
-        attrs.href = `#${attr[0].value}`;
-        const value = child[0].data;
-        return <a {...attrs}>{value}</a>;
-      }, [data]);
+  const renderContent = useMemo(() => {
+    // const attr = data?.attributes;
+    // const child = data?.childNodes;
+    // const attrs: any = {};
+    // attrs.href = `#${attr[0].value}`;
+    // const value = child[0].data;
+    // return <a {...attrs}>{value}</a>;
+
+    const attrs: any[] = data.attributes;
+    const xrefRow = Object.values(attrs).find(
+      (attr: any) => attr?.name === "target"
+    );
+    const attr: any = {};
+    attr.href = `#${xrefRow.value}`;
+    return <a className="xref" {...attr}><DisplayNode data={data.childNodes} /></a>
+  }, [data]);
 
   return <>{renderContent}</>;
 }
