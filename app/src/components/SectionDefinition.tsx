ac60d65459dc82ea03351340ce693acc9d0e3b5e
s4birli <sabirli31@gmail.com>
2021-12-07 15:17:43 +0000

    create the section definition component

---
+++ app/src/components/SectionDefinition.tsx
@@ -0,0 +1,25 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+import { useMemo } from "react";
+import DisplayNode from "./DisplayNode";
+import "./SectionDefinition.css";
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+interface OwnProps {
+  data: any;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default function SectionTerm({ data }: OwnProps) {
+  const renderContent = useMemo(() => {
+    const attrs: any[] = data.attributes;
+    const depthRow = Object.values(attrs).find(
+      (attr: any) => attr?.name === "id"
+    );
+    const id = depthRow?.value ? depthRow.value : "";
+    return <div className="definition" id={id}><DisplayNode data={data.childNodes} /></div>
+  }, [data]);
+
+  return <>{renderContent}</>;
+}
