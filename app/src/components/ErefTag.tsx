9cca1a63a7a7b3f61a5645b950d58a369a1927bf
s4birli <sabirli31@gmail.com>
2021-12-07 15:09:02 +0000

    create the eref component

---
+++ app/src/components/ErefTag.tsx
@@ -0,0 +1,30 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+import { useMemo } from "react";
+import DisplayNode from "./DisplayNode";
+import "./ErefTag.css";
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+interface OwnProps {
+  data: any;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default function ErefTag({ data }: OwnProps) {
+  const renderContent = useMemo(() => {
+
+    const attrs: any[] = data.attributes;
+    const xrefRow = Object.values(attrs).find(
+      (attr: any) => attr?.name === "bibitemid"
+    );
+    // const citeasRow = Object.values(attrs).find(
+    //   (attr: any) => attr?.name === "citeas"
+    // );
+    // attr.citeas = citeasRow.value;
+    const attr: any = {};
+    attr.href = `#${xrefRow.value}`;
+    return <a className="eref" {...attr}><DisplayNode data={data.childNodes} /></a>
+  }, [data]);
+
+  return <>{renderContent}</>;
+}
