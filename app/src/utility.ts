a3a59266a48636e96a611f25d99f290e34ef71b5
s4birli <sabirli31@gmail.com>
2021-12-08 19:25:59 +0000

    add the function to get the xml term data

--- app/src/utility.ts
+++ app/src/utility.ts
@@ -1,9 +1,50 @@
 import { DOMParser } from 'xmldom';
-import presentationData from "./data/document-presentation";
+import presentationData from './data/document-presentation';
 
 export const getChildsById = (id: string) => {
-    const xmlDoc = new DOMParser().parseFromString(presentationData,
+    const xmlDoc = new DOMParser().parseFromString(
+        presentationData,
         'text/xml'
     );
     return xmlDoc.getElementById(id);
+};
+
+export const getChildsByTagname = (name: string) => {
+    const xmlDoc = new DOMParser().parseFromString(
+        presentationData,
+        'text/xml'
+    );
+    return xmlDoc.getElementsByTagName(name);
+};
+
+export const getPlainText = (term: string) => {
+    if (term) {
+        let idText = term.substr(5);
+        return idText.split("-").length === 1 ?  idText : "";
+    } else {
+        return "";
+    }
 }
+
+export const getTerminologies = () => {
+    const tagElements = getChildsByTagname('term');
+    if (tagElements) {
+        const terms = Object.values(tagElements).map((element: any) => {
+            let attrs = element.attributes;
+            if (attrs) {
+                let idRow: any = Object.values(attrs).find(
+                    (attr: any) => attr?.name === 'id'
+                );
+                if (getPlainText(idRow.value))
+                    return {id: idRow?.value, text: getPlainText(idRow?.value)};
+                else 
+                    return {};
+            } else {
+                return {};
+            }
+        });
+        return terms.filter((term: any) => term.id);
+    } else {
+        return [];
+    }
+};
