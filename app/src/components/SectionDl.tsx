8a8bbd4140a24a77633a2a3909b1808bacef54b7
s4birli <sabirli31@gmail.com>
2021-12-13 12:22:21 +0000

    create the Dl tag

---
+++ app/src/components/SectionDl.tsx
@@ -0,0 +1,25 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+import { useMemo } from "react";
+import DisplayNode, { XMLNode } from "./DisplayNode";
+import "./SectionDl.css";
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+interface OwnProps {
+  data: any | XMLNode;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default function SectionDl({ data }: OwnProps) {
+  const renderContent = useMemo(() => {
+    const attrs: any[] = data.attributes;
+    const idRow = Object.values(attrs).find(
+      (attr: any) => attr?.name === "id"
+    );
+    const id = idRow?.value ? idRow.value : "";
+    return <dl className="dl" id={id}><DisplayNode data={data.childNodes} /></dl>
+  }, [data]);
+
+  return <>{renderContent}</>;
+}
