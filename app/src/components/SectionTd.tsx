d0ff17db84f98f09239016d5358bcc676843602c
s4birli <sabirli31@gmail.com>
2021-12-13 15:57:39 +0000

    create the td tag

---
+++ app/src/components/SectionTd.tsx
@@ -0,0 +1,30 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+import { useMemo } from "react";
+import DisplayNode, { XMLNode } from "./DisplayNode";
+import "./SectionTd.css";
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+interface OwnProps {
+  data: any | XMLNode;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default function SectionTd({ data }: OwnProps) {
+  const renderContent = useMemo(() => {
+    console.log(data, 'tddata');
+    const attrs: any = data.attributes;
+    const props: any = {};
+    const valignRow = Object.values(attrs).map(
+      (attr: any) => {
+        if (attr?.name) {
+          props[attr.name] = attr.value;
+        }
+      }
+    );
+    return <td className="td" {...props}><DisplayNode data={data.childNodes} /></td>
+  }, [data]);
+
+  return <>{renderContent}</>;
+}
