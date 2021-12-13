cf4cd7c5cfd5bca3864550a4dfb150851b7526c8
s4birli <sabirli31@gmail.com>
2021-12-13 15:10:35 +0000

    create the Dt tag

---
+++ app/src/components/SectionDt.tsx
@@ -0,0 +1,25 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+import { useMemo } from "react";
+import DisplayNode, { XMLNode } from "./DisplayNode";
+import "./SectionDt.css";
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+interface OwnProps {
+  data: any | XMLNode;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default function SectionDt({ data }: OwnProps) {
+  const renderContent = useMemo(() => {
+    const attrs: any[] = data.attributes;
+    const idRow = Object.values(attrs).find(
+      (attr: any) => attr?.name === "id"
+    );
+    const id = idRow?.value ? idRow.value : "";
+    return <dt className="dt" id={id}><DisplayNode data={data.childNodes} /></dt>
+  }, [data]);
+
+  return <>{renderContent}</>;
+}
