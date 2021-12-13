0e15a17ffc463fdc93b6dbee2e1e470903cb0fd4
s4birli <sabirli31@gmail.com>
2021-12-13 09:08:07 +0000

    create the table tag

---
+++ app/src/components/SectionTable.tsx
@@ -0,0 +1,25 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+import { useMemo } from "react";
+import DisplayNode, { XMLNode } from "./DisplayNode";
+import "./SectionTable.css";
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+interface OwnProps {
+  data: any | XMLNode;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default function SectionTable({ data }: OwnProps) {
+  const renderContent = useMemo(() => {
+    const attrs: any[] = data.attributes;
+    const idRow = Object.values(attrs).find(
+      (attr: any) => attr?.name === "id"
+    );
+    const id = idRow?.value ? idRow.value : "";
+    return <div className="table" id={id}><DisplayNode data={data.childNodes} /></div>
+  }, [data]);
+
+  return <>{renderContent}</>;
+}
