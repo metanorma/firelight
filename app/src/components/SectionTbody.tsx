3ca5df4cb4ae41f6c2a8b34146241702621776a0
s4birli <sabirli31@gmail.com>
2021-12-13 15:11:11 +0000

    create the tbody tag

---
+++ app/src/components/SectionTbody.tsx
@@ -0,0 +1,25 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+import { useMemo } from "react";
+import DisplayNode, { XMLNode } from "./DisplayNode";
+import "./SectionThead.css";
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+interface OwnProps {
+  data: XMLNode;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default function SectionTbody({ data }: OwnProps) {
+  const renderContent = useMemo(() => {
+    const attrs: any[] = data.attributes;
+    const idRow = Object.values(attrs).find(
+      (attr: any) => attr?.name === "id"
+    );
+    const id = idRow?.value ? idRow.value : "";
+    return <tbody className="tbody" id={id}><DisplayNode data={data.childNodes} /></tbody>
+  }, [data]);
+
+  return <>{renderContent}</>;
+}
