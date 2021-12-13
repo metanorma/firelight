6206b532b8e2b732fbc6b60ad809daabfaa00086
s4birli <sabirli31@gmail.com>
2021-12-13 12:22:00 +0000

    create the Dd tag

---
+++ app/src/components/SectionDd.tsx
@@ -0,0 +1,25 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+import { useMemo } from "react";
+import DisplayNode, { XMLNode } from "./DisplayNode";
+import "./SectionDl.css";
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+interface OwnProps {
+  data: any | XMLNode;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default function SectionDl({ data }: OwnProps) {
+  const renderContent = useMemo(() => {
+    const attrs: any[] = data.attributes;
+    const idRow = Object.values(attrs).find(
+      (attr: any) => attr?.name === "id"
+    );
+    const id = idRow?.value ? idRow.value : "";
+    return <dd className="dd" id={id}><DisplayNode data={data.childNodes} /></dd>
+  }, [data]);
+
+  return <>{renderContent}</>;
+}
