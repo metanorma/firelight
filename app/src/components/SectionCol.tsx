069de0dd2ba8cebaad65658124584649c89af2d3
s4birli <sabirli31@gmail.com>
2021-12-13 09:23:21 +0000

    create the colgroup, col tag

---
+++ app/src/components/SectionCol.tsx
@@ -0,0 +1,24 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+import { useMemo } from "react";
+import DisplayNode, { XMLNode } from "./DisplayNode";
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+interface OwnProps {
+  data: any | XMLNode;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default function SectionColgroup({ data }: OwnProps) {
+  const renderContent = useMemo(() => {
+    const attrs: any[] = data.attributes;
+    const idRow = Object.values(attrs).find(
+      (attr: any) => attr?.name === "width"
+    );
+    const width = idRow?.value ? idRow.value : "";
+    return <col width={width}></col>
+  }, [data]);
+
+  return <>{renderContent}</>;
+}
