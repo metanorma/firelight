5af0f7e1f6e998fa8dda325b0c7c6c9a0d9d53b5
s4birli <sabirli31@gmail.com>
2021-12-13 09:58:11 +0000

    create the thead tag

---
+++ app/src/components/SectionThead.tsx
@@ -0,0 +1,25 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+import { useMemo } from "react";
+import DisplayNode, { XMLNode } from "./DisplayNode";
+import "./SectionThead.css";
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+interface OwnProps {
+  data: XMLNode;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default function SectionThead({ data }: OwnProps) {
+  const renderContent = useMemo(() => {
+    const attrs: any[] = data.attributes;
+    const idRow = Object.values(attrs).find(
+      (attr: any) => attr?.name === "id"
+    );
+    const id = idRow?.value ? idRow.value : "";
+    return <thead className="thead" id={id}><DisplayNode data={data.childNodes} /></thead>
+  }, [data]);
+
+  return <>{renderContent}</>;
+}
