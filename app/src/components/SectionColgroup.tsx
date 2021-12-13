0a6d026f7f7fe333bf8b1eaea905713490d69de5
s4birli <sabirli31@gmail.com>
2021-12-13 09:23:21 +0000

    create the colgroup, col tag

---
+++ app/src/components/SectionColgroup.tsx
@@ -0,0 +1,19 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+import { useMemo } from "react";
+import DisplayNode, { XMLNode } from "./DisplayNode";
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+interface OwnProps {
+  data: any | XMLNode;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default function SectionColgroup({ data }: OwnProps) {
+  const renderContent = useMemo(() => {
+    return <colgroup><DisplayNode data={data.childNodes} /></colgroup>
+  }, [data]);
+
+  return <>{renderContent}</>;
+}
