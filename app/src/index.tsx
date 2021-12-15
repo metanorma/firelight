11f89e213da2d79740958ffea0c0df55d212eb18
s4birli <sabirli31@gmail.com>
2021-12-15 21:56:45 +0000

    add the router provider in index.tsx

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,17 +2,16 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-    BrowserRouter as Router
-  } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 import './css/fireball.css';
 ReactDOM.render(
     <React.StrictMode>
-        <Router><App /></Router>
-        
+        <Router>
+            <App />
+        </Router>
     </React.StrictMode>,
     document.getElementById('root')
 );
