77071f16643079569d1cbd8e93a69b0450ced3e9
s4birli <sabirli31@gmail.com>
2021-12-17 21:30:31 +0000

    add the xml provider

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,15 +3,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import XmlProvider from './context';
 import App from './App';
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 import './css/fireball.css';
 ReactDOM.render(
     <React.StrictMode>
-        <Router>
-            <App />
-        </Router>
+        <XmlProvider>
+            <Router>
+                <App />
+            </Router>
+        </XmlProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
