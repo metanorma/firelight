25215bca3a42254f2970e1cd45f9e2df74f407f7
s4birli <sabirli31@gmail.com>
2022-01-05 22:40:24 +0000

    create the main page

--- app/src/components/MainPage.tsx
+++ app/src/components/MainPage.tsx
@@ -18,62 +18,77 @@ export default function MainPage() {
 
     // split the xml data by content section and save those as array
     const contentSections = useMemo(() => {
+        let index = 0;
         const getMenuItem = (data: any): any => {
             const returnData: any = {};
-            returnData.index = data['$']['displayorder'];
+            // returnData.index = data['$']['displayorder'];
+            returnData.index = index++;
             returnData.data = data;
             return returnData;
         };
 
         const menuItem: any[] = [];
-        if (xmlJson['bsi-standard']) {
-            //the foreword part
-            const foreword = getMenuItem(
-                xmlJson['bsi-standard']['preface'][0]['foreword'][0]
-            );
-            menuItem[foreword.index] = foreword;
-            // menuItem.push(foreword);
-            //the introduction part
-            const introduction = getMenuItem(
-                xmlJson['bsi-standard']['preface'][0]['introduction'][0]
-            );
-            menuItem[introduction.index] = introduction;
-            // menuItem.push(introduction);
-            //the introduction part
-            const references = getMenuItem(
-                xmlJson['bsi-standard']['bibliography'][0]['references'][0]
-            );
-            menuItem[references.index] = references;
-            // menuItem.push(references);
-            //the terms part
-            const terms = getMenuItem(
-                xmlJson['bsi-standard']['sections'][0]['terms'][0]
-            );
-            menuItem[terms.index] = terms;
-            // menuItem.push(terms)
-            // the sction part
-            const sections = xmlJson['bsi-standard']['sections'][0]['clause'];
-            if (sections?.length) {
-                sections.map((sectoin: any) => {
-                    const sectionItem = getMenuItem(sectoin);
-                    menuItem[sectionItem.index] = sectionItem;
-                    // menuItem.push(sectionItem);
-                });
+        if (xmlJson['iso-standard']) {
+            //the foreword part for menu item
+            if (xmlJson['iso-standard']['preface']) {
+                const foreword = getMenuItem(
+                    xmlJson['iso-standard']['preface'][0]['foreword'][0]
+                );
+                menuItem[foreword.index] = foreword;
             }
-            //the sction part
-            const annex = xmlJson['bsi-standard']['annex'];
-            if (annex?.length) {
-                annex.map((sectoin: any) => {
-                    const sectionItem = getMenuItem(sectoin);
-                    menuItem[sectionItem.index] = sectionItem;
-                    // menuItem.push(sectionItem)
-                });
+            //the introduction part for menu item
+            if (xmlJson['iso-standard']['preface']) {
+                const introduction = getMenuItem(
+                    xmlJson['iso-standard']['preface'][0]['introduction'][0]
+                );
+                menuItem[introduction.index] = introduction;
+            }
+            //the scopt part for menu
+            if (xmlJson['iso-standard']['sections'] ) {
+                const sectionItem = getMenuItem(
+                    xmlJson['iso-standard']['sections'][0]['clause'][0]
+                );
+                menuItem[sectionItem.index] = sectionItem;
+            }
+            //the normative part for menu item
+            if (xmlJson['iso-standard']['bibliography']) {
+                const references = getMenuItem(
+                    xmlJson['iso-standard']['bibliography'][0]['references'][0]
+                );
+                menuItem[references.index] = references;
+            }
+            //the terms part for menu item
+            if (xmlJson['iso-standard']['sections']) {
+                const terms = getMenuItem(
+                    xmlJson['iso-standard']['sections'][0]['terms'][0]
+                );
+                menuItem[terms.index] = terms;
+            }
+            //the section part for menu item
+            if (xmlJson['iso-standard']['sections'] ) {
+                const sectionItem = getMenuItem(
+                    xmlJson['iso-standard']['sections'][0]['clause'][1]
+                );
+                menuItem[sectionItem.index] = sectionItem;
+            }
+            //the sction part for menu item
+            if (xmlJson['iso-standard']['annex']) {
+                const annex = xmlJson['iso-standard']['annex'];
+                if (annex?.length) {
+                    annex.map((sectoin: any) => {
+                        const sectionItem = getMenuItem(sectoin);
+                        menuItem[sectionItem.index] = sectionItem;
+                        // menuItem.push(sectionItem)
+                    });
+                }
             }
             //the bibliography part
-            const bibliography = getMenuItem(
-                xmlJson['bsi-standard']['bibliography'][0]['clause'][0]
-            );
-            menuItem[bibliography.index] = bibliography;
+            if (xmlJson['iso-standard']['bibliography']) {
+                const references = getMenuItem(
+                    xmlJson['iso-standard']['bibliography'][0]['references'][1]
+                );
+                menuItem[references.index] = references;
+            }
         }
         const resultArray: any[] = [];
         menuItem.map((item: any) => {
