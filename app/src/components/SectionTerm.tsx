62aac84ee48e5cdb3015762aa1eaf070c60b54d2
s4birli <sabirli31@gmail.com>
2022-01-06 23:04:15 +0000

    update the term tag based on iso term tag

--- app/src/components/SectionTerm.tsx
+++ app/src/components/SectionTerm.tsx
@@ -1,25 +1,75 @@
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-import { useMemo } from "react";
-import DisplayNode, { XMLNode } from "./DisplayNode";
-import "./SectionP.css";
+import { useMemo } from 'react';
+import DisplayNode, { XMLNode } from './DisplayNode';
+import './SectionTerm.css';
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 interface OwnProps {
-  data: XMLNode;
+    data: XMLNode;
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 export default function SectionTerm({ data }: OwnProps) {
-  const renderContent = useMemo(() => {
-    const attrs: any[] = data.attributes;
-    const idRow = Object.values(attrs).find(
-      (attr: any) => attr?.name === "id"
-    );
-    const id = idRow?.value ? idRow.value : "";
-    return <div className="term" id={id}><DisplayNode data={data.childNodes} /></div>
-  }, [data]);
+    const renderContent = useMemo(() => {
+        const attrs: any[] = data.attributes;
+        const idRow = Object.values(attrs).find(
+            (attr: any) => attr?.name === 'id'
+        );
+        const id = idRow?.value ? idRow.value : '';
 
-  return <>{renderContent}</>;
+        const childs: any[] = data.childNodes;
+        const termSource = Object.values(childs).find(
+            (child: any) => child?.tagName === 'termsource'
+        );
+
+        let modified = '';
+        let bibitemid = '';
+        let bibitemText = '';
+        let referenceIndex = '';
+
+        if (termSource?.childNodes) {
+            let origin: any = Object.values(termSource.childNodes).find(
+              (child: any) => child?.tagName === 'origin'
+            )
+            let attr = origin?.attributes;
+            
+            let reference: any = Object.values(origin?.attributes).find(
+              (attr: any) => attr?.nodeName === 'bibitemid'
+            )
+            bibitemid = reference?.value;
+            
+            reference = Object.values(origin?.attributes).find(
+              (attr: any) => attr?.nodeName === 'citeas'
+            )
+            bibitemText = reference?.value;
+
+            referenceIndex = origin?.childNodes[0].childNodes[0].childNodes[0].childNodes[0].data;
+
+            let modification: any = Object.values(termSource.childNodes).find(
+              (child: any) => child?.tagName === 'modification'
+            );
+            
+            if (modification?.childNodes) {
+              let p: any = Object.values(modification?.childNodes).find(
+                (child: any) => child?.tagName === 'p'
+              )
+              let value: string | any = p?.childNodes[0].data;
+              if (value) {
+                modified = `modified - ${value}`;
+                console.log(modified, 'modified')
+              }
+            }
+        }
+
+        return (
+            <div className="term" id={id}>
+                <DisplayNode data={data.childNodes} />
+                <div className="termsource">[SOURCE: <a href={ bibitemid ? `#${bibitemid}` : ''}>{bibitemText}</a>{ modified ? `, ${modified}` : ''}]</div>
+            </div>
+        );
+    }, [data]);
+
+    return <>{renderContent}</>;
 }
