58c4fba9fab3b03b77bd2f8a064214d2a5087c7e
s4birli <sabirli31@gmail.com>
2022-01-06 23:01:20 +0000

    create the section admitted component

---
+++ app/src/components/SectionAdmitted.tsx
@@ -0,0 +1,25 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+import { useMemo } from "react";
+import DisplayNode, { XMLNode } from "./DisplayNode";
+import "./SectionAdmitted.css";
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+interface OwnProps {
+  data: XMLNode;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default function SectionAdmitted({ data }: OwnProps) {
+  const renderContent = useMemo(() => {
+    const attrs: any[] = data.attributes;
+    const idRow = Object.values(attrs).find(
+      (attr: any) => attr?.name === "id"
+    );
+    const id = idRow?.value ? idRow.value : "";
+    return <div className="admitted" id={id}><DisplayNode data={data.childNodes} /></div>
+  }, [data]);
+
+  return <>{renderContent}</>;
+}
