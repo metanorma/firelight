65b923f53d721fc908537cc38dfda075deb070a5
s4birli <sabirli31@gmail.com>
2022-01-07 17:54:15 +0000

    update the LinkTag

--- app/src/components/LinkTag.tsx
+++ app/src/components/LinkTag.tsx
@@ -11,13 +11,16 @@ interface OwnProps {
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 export default function LinkTag({ data }: OwnProps) {
-    const renderContent = useMemo(() => {
+    const renderContent = useMemo(() => { console.log(data, 'link');
         const attr = data?.attributes;
         const child = data?.childNodes;
         const attrs: any = {};
         attrs.href = attr[0].value;
         let value = '';
         if(child.length && child[0].data) value = child[0].data;
+        if (!value) {
+          value = attrs.href.split(":")[1];
+        }
         return <a {...attrs} key={attrs.href} className="link">{value}</a>;
       }, [data]);
 
