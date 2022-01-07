49c4925bb834070c96c1542f578716aec2982351
s4birli <sabirli31@gmail.com>
2022-01-07 15:59:44 +0000

    create ul tag component

---
+++ app/src/components/SectionUl.tsx
@@ -0,0 +1,38 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+import { useMemo } from "react";
+import DisplayNode, { XMLNode } from "./DisplayNode";
+import "./SectionUl.css";
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+interface OwnProps {
+  data: string | XMLNode;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default function SectionUl({ data }: OwnProps) {
+  const renderContent = useMemo(() => {
+    if (typeof data === "string") return <ol>{data}</ol>;
+    const attrs: any[] = data.attributes;
+    let dataRow = Object.values(attrs).find((attr: any) => attr?.name === "id");
+    const id = dataRow?.value ? dataRow.value : "";
+    // dataRow = Object.values(attrs).find((attr: any) => attr?.name === "type");
+    // const type: any = dataRow?.value === "alphabet" ? "A" : "1";
+    return (
+      <ul className="ul">
+        {Object.values(data.childNodes).length > 0 &&
+          Object.values(data.childNodes).map(
+            (item: any, index: number) =>
+              item.tagName === "li" && (
+                <li key={index}>
+                  <DisplayNode data={item.childNodes} />
+                </li>
+              )
+          )}
+      </ul>
+    );
+  }, [data]);
+
+  return <>{renderContent}</>;
+}
