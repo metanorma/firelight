e8d15a4cf5eb172f07c53515c91103aef4b00ad7
s4birli <sabirli31@gmail.com>
2022-01-07 20:38:55 +0000

    add the index number for title

--- app/src/components/ContentSection.tsx
+++ app/src/components/ContentSection.tsx
@@ -6,11 +6,12 @@ import DisplayNode from './DisplayNode';
 
 interface OwnProps {
     xmlData: any;
+    titleIndex?: string | any;
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-export default function ContentSection({ xmlData }: OwnProps) {
+export default function ContentSection({ xmlData, titleIndex }: OwnProps) {
     const renderContent = useMemo(() => {
         const id = xmlData?.$?.id ? xmlData.$.id : '';
         let node: any = '';
@@ -18,22 +19,51 @@ export default function ContentSection({ xmlData }: OwnProps) {
             node = getChildsById(id);
         }
 
-        return node ? (
-            <div className="content-section" id={id}>
-                <DisplayNode data={node.childNodes} />
-                {xmlData?.clause?.length > 0 &&
-                    xmlData.clause.map((child: any, index: number) => (
-                        <ContentSection key={index} xmlData={child} />
-                    ))}
-            </div>
-        ) : (
-            <>
-                {xmlData?.clause?.length > 0 &&
-                    xmlData.clause.map((child: any, index: number) => (
-                        <ContentSection key={index} xmlData={child} />
-                    ))}
-            </>
-        );
+        if (!titleIndex) {
+            return node ? (
+                <div className="content-section" id={id}>
+                    <DisplayNode data={node.childNodes} />
+                    {xmlData?.clause?.length > 0 &&
+                        xmlData.clause.map((child: any, index: number) => (
+                            <ContentSection key={index} xmlData={child} />
+                        ))}
+                </div>
+            ) : (
+                <>
+                    {xmlData?.clause?.length > 0 &&
+                        xmlData.clause.map((child: any, index: number) => (
+                            <ContentSection key={index} xmlData={child} />
+                        ))}
+                </>
+            );
+        } else {
+            console.log(xmlData?.title, 'title');
+            if (xmlData?.title && xmlData?.title[0]) {
+                let title = xmlData.title[0];
+                title = `${titleIndex} ${title}`;
+                if (node) {
+                    delete node.childNodes[0];
+                    return (
+                        <div className="content-section" id={id}>
+                            <h1 className="title title-3">{title}</h1>
+                            <DisplayNode data={node.childNodes} />
+                            {xmlData?.clause?.length > 0 &&
+                                xmlData.clause.map(
+                                    (child: any, index: number) => (
+                                        <ContentSection
+                                            key={index}
+                                            xmlData={child}
+                                            titleIndex={`${titleIndex}.${
+                                                index + 1
+                                            }`}
+                                        />
+                                    )
+                                )}
+                        </div>
+                    );
+                }
+            }
+        }
     }, [xmlData]);
 
     return <>{renderContent}</>;
