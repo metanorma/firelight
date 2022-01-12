d2c4e66ed46e1db46dcccffafdbf83778845ecc3
s4birli <sabirli31@gmail.com>
2022-01-12 18:35:20 +0000

    add the TermExample tag

---
+++ app/src/components/SectionTermexample.tsx
@@ -0,0 +1,25 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+import { useMemo } from "react";
+import DisplayNode, { XMLNode } from "./DisplayNode";
+import "./SectionTermexample.css";
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+interface OwnProps {
+  data: XMLNode;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default function SectionTerm({ data }: OwnProps) {
+  const renderContent = useMemo(() => {
+    const attrs: any[] = data.attributes;
+    const idRow = Object.values(attrs).find(
+      (attr: any) => attr?.name === "id"
+    );
+    const id = idRow?.value ? idRow.value : "";
+    return <div className="termexample" id={id}><span>Example</span><DisplayNode data={data.childNodes} /></div>
+  }, [data]);
+
+  return <>{renderContent}</>;
+}
