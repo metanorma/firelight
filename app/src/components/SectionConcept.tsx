e1ca266d6e67bbec50982b2a9de0413b64a48bd1
s4birli <sabirli31@gmail.com>
2022-01-12 18:33:58 +0000

    add the concept tag

---
+++ app/src/components/SectionConcept.tsx
@@ -0,0 +1,27 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+import { useMemo } from "react";
+import DisplayNode, { XMLNode } from "./DisplayNode";
+import "./SectionConcept.css";
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+interface OwnProps {
+  data: XMLNode;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default function SectionDomain({ data }: OwnProps) {
+  const renderContent = useMemo(() => { console.log(data, 'concept')
+    const attrs: any[] = data.attributes;
+    const idRow = Object.values(attrs).find(
+      (attr: any) => attr?.name === "id"
+    );
+    const id = idRow?.value ? idRow.value : "";
+
+    const href = data.childNodes[0].childNodes[0].data;
+    return <div className="concept" id={id}>(<a href={`#${href}`}>{href}</a>)</div>
+  }, [data]);
+
+  return <>{renderContent}</>;
+}
