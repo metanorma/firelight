6744d0d571f76fc00c790e608ebeaf1efecc7127
s4birli <sabirli31@gmail.com>
2022-01-14 19:55:51 +0000

    create the formula tag

---
+++ app/src/components/SectionFormula.tsx
@@ -0,0 +1,30 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+import { useMemo } from 'react';
+import DisplayNode, { XMLNode } from './DisplayNode';
+import './SectionFormula.css';
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+interface OwnProps {
+    data: XMLNode;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default function SectionFormula({ data }: OwnProps) {
+    const renderContent = useMemo(() => {
+        const attrs: any[] = data.attributes;
+        const idRow = Object.values(attrs).find(
+            (attr: any) => attr?.name === 'id'
+        );
+        const id = idRow?.value ? idRow.value : '';
+
+        return (
+            <div className="formula" id={id}>
+                <DisplayNode data={data.childNodes} />
+            </div>
+        );
+    }, [data]);
+
+    return <>{renderContent}</>;
+}
