a1c8a0cc565416f8a91fe6d49a559c49d4e13a41
s4birli <sabirli31@gmail.com>
2022-01-19 15:46:11 +0000

    create the math tag using react-math-jsx

--- app/src/components/SectionMath.tsx
+++ app/src/components/SectionMath.tsx
@@ -1,8 +1,9 @@
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 import { useMemo } from 'react';
-import DisplayNode, { XMLNode } from './DisplayNode';
-import './SectionMath.css';
+import { XMLNode } from './DisplayNode';
+// import './SectionMath.css';
+import MathJSX from 'react-mathjax-preview';
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 interface OwnProps {
@@ -10,20 +11,35 @@ interface OwnProps {
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+const makeMathJSX = (data: any) => {
+    let mathString = '';
+    const attrs = data?.attributes;
+    const childNodes = data?.childNodes;
+    if (data?.tagName) {
+        mathString += `<${data?.tagName}`;
+        if (attrs) {
+            Object.values(attrs).map((child: any) => {
+                if (child?.name) mathString += ` ${child.name}`;
+                if (child?.value) mathString += `="${child.value}"`;
+            });
+        }
+        mathString += '>';
+        if (childNodes) {
+            Object.values(childNodes).map((child: any) => {
+                mathString += makeMathJSX(child);
+            });
+        }
+        mathString += `</${data.tagName}>`;
+        return mathString;
+    } else {
+        return data?.data ? data?.data : '';
+    }
+};
 
 export default function SectionMath({ data }: OwnProps) {
     const renderContent = useMemo(() => {
-        const attrs: any[] = data.attributes;
-        const idRow = Object.values(attrs).find(
-            (attr: any) => attr?.name === 'id'
-        );
-        const id = idRow?.value ? idRow.value : '';
-
-        return (
-            <div className="math" id={id}>
-                <DisplayNode data={data.childNodes} />
-            </div>
-        );
+        const math = makeMathJSX(data);
+        return <MathJSX math={math} />;
     }, [data]);
 
     return <>{renderContent}</>;
