087ea6b5876f08d644e6d356bab19fa00754babb
s4birli <sabirli31@gmail.com>
2022-01-21 15:37:39 +0000

    fixing the bibliography for itu-standard

--- app/src/components/MainPage.tsx
+++ app/src/components/MainPage.tsx
@@ -88,8 +88,8 @@ export default function MainPage() {
                     });
                 }
                 //bibliography part
-                if (xmlJson[standard]['bibliography']) {
-                    xmlJson[standard]['bibliography'].map((data: any) => {
+                if (xmlJson[standard]['bibliography'][0]['references']) {
+                    xmlJson[standard]['bibliography'][0]['references'].map((data: any) => {
                         const item = getMenuItem(data);
                         if (item) menuItem[item.index] = item;
                     });
