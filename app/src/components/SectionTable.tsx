443769ecacea2f1039c0b9f82b02c032334590d4
s4birli <sabirli31@gmail.com>
2022-01-24 14:54:46 +0000

    create the new table title for 'fig

--- app/src/components/SectionTable.tsx
+++ app/src/components/SectionTable.tsx
@@ -1,25 +1,62 @@
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-import { useMemo } from "react";
-import DisplayNode, { XMLNode } from "./DisplayNode";
-import "./SectionTable.css";
+import { useMemo } from 'react';
+import DisplayNode, { XMLNode } from './DisplayNode';
+import './SectionTable.css';
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 interface OwnProps {
-  data: any | XMLNode;
+    data: any | XMLNode;
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 export default function SectionTable({ data }: OwnProps) {
-  const renderContent = useMemo(() => {
-    const attrs: any[] = data.attributes;
-    const idRow = Object.values(attrs).find(
-      (attr: any) => attr?.name === "id"
-    );
-    const id = idRow?.value ? idRow.value : "";
-    return <table className="table" id={id}><DisplayNode data={data.childNodes} /></table>
-  }, [data]);
+    const renderContent = useMemo(() => {
+        const attrs: any[] = data.attributes;
+        const idRow = Object.values(attrs).find(
+            (attr: any) => attr?.name === 'id'
+        );
+        const id = idRow?.value ? idRow.value : '';
+        
+        if (id === 'fig5') console.log(data, 'fig5')
 
-  return <>{renderContent}</>;
+        if (id?.toLowerCase().includes('fig')) {
+            let name = '';
+            let nameRow: any = Object.values(data.childNodes).find(
+                (child: any) => child?.tagName === 'name'
+            );
+            if (nameRow) {
+                name = nameRow.childNodes[0].data;
+                let index = '';
+                if (isNaN(parseInt(id.substring(3)))) {
+                    index =
+                        'Figure ' +
+                        id.substring(4).substring(0, 1).toUpperCase() +
+                        id.substring(4).substring(1).toUpperCase();
+                } else {
+                    index = 'Figure ' + id.substring(3);
+                }
+                if (index) name = index + ' — ' + name;
+            }
+
+            let nodes = Object.values(data?.childNodes).filter(
+              (child: any, index: number) => child?.tagName !== 'name'
+            )
+            return (
+                <table className="table" id={id}>
+                    <DisplayNode data={nodes} />
+                    {name && <div className="name">{name ? name : ''}</div>}
+                </table>
+            );
+        }
+
+        return (
+            <table className="table" id={id}>
+                <DisplayNode data={data.childNodes} />
+            </table>
+        );
+    }, [data]);
+
+    return <>{renderContent}</>;
 }
