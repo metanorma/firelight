52bcdb4785d3929451f4f99b41527a9b8a1368c3
s4birli <sabirli31@gmail.com>
2022-01-27 14:50:18 +0000

    add the feature for table title

--- app/src/components/SectionTable.tsx
+++ app/src/components/SectionTable.tsx
@@ -51,6 +51,36 @@ export default function SectionTable({ data }: OwnProps) {
             );
         }
 
+        if (id?.toLowerCase().includes('table')) {
+            let name = '';
+            let nameRow: any = Object.values(data.childNodes).find(
+                (child: any) => child?.tagName === 'name'
+            );
+            if (nameRow) {
+                name = nameRow.childNodes[0].data;
+                let index = '';
+                if (isNaN(parseInt(id.substring(3)))) {
+                    index =
+                        'Table ' +
+                        id.substring(5).substring(0, 1).toUpperCase() +
+                        id.substring(5).substring(1).toUpperCase();
+                } else {
+                    index = 'Table ' + id.substring(5);
+                }
+                if (index) name = index + ' — ' + name;
+            }
+
+            let nodes = Object.values(data?.childNodes).filter(
+              (child: any, index: number) => child?.tagName !== 'name'
+            )
+            return (
+                <table className="table" id={id}>
+                    {name && <div className="name">{name ? name : ''}</div>}
+                    <DisplayNode data={nodes} />
+                </table>
+            );
+        }
+
         return (
             <table className="table" id={id}>
                 <DisplayNode data={data.childNodes} />
