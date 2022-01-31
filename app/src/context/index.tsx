6b4b025365a041f99b5636b4a37f7c53b859e34f
s4birli <sabirli31@gmail.com>
2022-01-31 13:15:31 +0000

    get the title in the case that there is no main title

--- app/src/context/index.tsx
+++ app/src/context/index.tsx
@@ -12,7 +12,7 @@ export type XmlData = {
 const contextDefaultValues: XmlData = {
     xml: '',
     xmlJson: {},
-    title: '',
+    title: ''
 };
 
 export const XmlContext = createContext<XmlData>(contextDefaultValues);
@@ -21,13 +21,18 @@ export const XmlContext = createContext<XmlData>(contextDefaultValues);
 // const xmlUrl = "https://metanorma.github.io/mn-samples-iso/documents/international-workshop-agreement/document.xml";
 // const xmlUrl = "/iso.xml";
 // const xmlUrl = "https://metanorma.github.io/mn-samples-iso/documents/international-standard/rice-en.wd.xml";
-// const xmlUrl0 = "/rice.xml";
-const xmlUrl = "https://metanorma.github.io/mn-samples-itu/documents/T-TUT-CCIT-2015-E.xml";
-const localXmlUrl = "/a2015.xml";
-const xmlUrl1 = "https://metanorma.github.io/mn-samples-itu/documents/T-TUT-L-2020-GLR.xml";
-const localXmlUrl1 = "/2020.xml";
-const xmlUrl2 = "https://metanorma.github.io/mn-samples-itu/documents/T-REC-A.8-200810-I!!MSW-E.xml";
-const localXmlUrl2 = "/2008.xml";
+const xmlUrl0 = '/rice.xml';
+const xmlUrl =
+    'https://metanorma.github.io/mn-samples-itu/documents/T-TUT-CCIT-2015-E.xml';
+const localXmlUrl = '/a2015.xml';
+const xmlUrl1 =
+    'https://metanorma.github.io/mn-samples-itu/documents/T-TUT-L-2020-GLR.xml';
+const localXmlUrl1 = '/2020.xml';
+const xmlUrl2 =
+    'https://metanorma.github.io/mn-samples-itu/documents/T-REC-A.8-200810-I!!MSW-E.xml';
+const localXmlUrl2 = '/2008.xml';
+const ogcUrl =
+    'https://metanorma.github.io/mn-samples-ogc/documents/12-077r1/document.xml';
 
 const XmlProvider: React.FC = ({ children }) => {
     const [xml, setXml] = useState<string>('');
@@ -36,47 +41,54 @@ const XmlProvider: React.FC = ({ children }) => {
 
     useEffect(() => {
         axios
-            .get(xmlUrl, {
+            .get(ogcUrl, {
                 headers: {
                     Accept: 'application/xml'
                 }
             })
             .then((response) => {
-                console.log(response.data, 'xml')
+                console.log(response.data, 'xml');
                 if (response.data) {
                     localStorage.setItem('xml', response.data);
                     setXml(response.data);
 
                     const xmlDoc = new DOMParser().parseFromString(
                         response.data,
-                        'text/xml'
+                        'application/xml'
                     );
 
                     const bibdata: XMLNode =
                         xmlDoc.getElementsByTagName('bibdata')[0];
                     if (bibdata?.childNodes) {
                         const childs = bibdata.childNodes;
-    
-                        const mainTitle: XMLNode | any = Object.values(childs).find(
-                            (child: any) => {
-                                if (child.tagName === 'title') {
-                                    let attrs = child.attributes;
-                                    let result = false;
-                                    Object.values(attrs).map((attr: any) => {
-                                        if (
-                                            attr.name === 'type' &&
-                                            attr.value === 'main'
-                                        ) {
-                                            result = true;
-                                        }
-                                    });
-                                    return result;
-                                } else {
-                                    return;
-                                }
+
+                        const mainTitle: XMLNode | any = Object.values(
+                            childs
+                        ).find((child: any) => {
+                            if (child.tagName === 'title') {
+                                let attrs = child.attributes;
+                                let result = false;
+                                Object.values(attrs).map((attr: any) => {
+                                    if (
+                                        attr.name === 'type' &&
+                                        attr.value === 'main'
+                                    ) {
+                                        result = true;
+                                    }
+                                });
+                                return result;
+                            } else {
+                                return;
                             }
-                        );
-                        setTitle(mainTitle?.childNodes[0].data);
+                        });
+
+                        if (mainTitle) setTitle(mainTitle?.childNodes[0].data);
+                        else {
+                            let mainTitle: XMLNode | any = Object.values(
+                                childs
+                            ).find((child: any) => child?.tagName === 'title');
+                            if (mainTitle) setTitle(mainTitle?.childNodes[0].data);
+                        }
                     } else {
                         setTitle('');
                     }
