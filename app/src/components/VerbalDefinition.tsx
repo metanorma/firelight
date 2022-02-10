9c6e75fd42ff79efa0edfe3a3216af4f24523d6a
s4birli <sabirli31@gmail.com>
2022-02-10 15:42:18 +0000

    create the VerbalDefinition component

---
+++ app/src/components/VerbalDefinition.tsx
@@ -0,0 +1,21 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+import { useMemo } from "react";
+import DisplayNode, { XMLNode } from "./DisplayNode";
+import "./VerbalDefinition.css";
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+interface OwnProps {
+  data: XMLNode;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default function VerbalDefinition({ data }: OwnProps) {
+    const renderContent = useMemo(() => {
+        if (typeof data === 'string') return <strong>{data}</strong>
+        return <div className="verbal-definition"><DisplayNode data={data.childNodes} /></div>;
+      }, [data]);
+
+  return <>{renderContent}</>;
+}
+
