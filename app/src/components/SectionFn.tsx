276d344664b38be0ed98ea9296bf4b3cb26d84e9
s4birli <sabirli31@gmail.com>
2022-02-17 20:13:02 +0000

    update the fn component

--- app/src/components/SectionFn.tsx
+++ app/src/components/SectionFn.tsx
@@ -25,12 +25,18 @@ export default function SectionFn({ data }: OwnProps) {
         );
         const reference = referenceRow?.value ? referenceRow.value : '';
 
+        let footnote: any = localStorage.getItem('footnote');
+        
+        let num = 0;
+        if (!footnote) num = 1;
+        else num = parseInt(footnote) + 1;
+
         return (
             <>
                 <a className="fn" href={`#table${reference}`}>
                     {reference}
                 </a>
-                <aside id={`table${reference}`} className="footnote">
+                <aside id={`table${reference}`} className="footnote" style={{bottom:`${num  * -40 } px`}}>
                     <p>
                         <a className="foot-note-ref" href={`#table${reference}`}><sup>{reference ? reference: ''}</sup></a>
                         <DisplayNode data={data.childNodes} />
