858c192272b1c27ab71068753fb6d2d72f1513cc
s4birli <sabirli31@gmail.com>
2022-02-25 22:19:28 +0000

    create the example component

---
+++ app/src/components/SectionAdmittedExample.tsx
@@ -0,0 +1,25 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+import { useMemo } from "react";
+import DisplayNode, { XMLNode } from "./DisplayNode";
+import "./Section Example.css";
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+interface OwnProps {
+  data: XMLNode;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default function Section Example({ data }: OwnProps) {
+  const renderContent = useMemo(() => {
+    const attrs: any[] = data.attributes;
+    const idRow = Object.values(attrs).find(
+      (attr: any) => attr?.name === "id"
+    );
+    const id = idRow?.value ? idRow.value : "";
+    return <div className=" Example" id={id}><DisplayNode data={data.childNodes} /></div>
+  }, [data]);
+
+  return <>{renderContent}</>;
+}
