c9a4170f01b189e038a0acf79a7fc427fe280649
s4birli <sabirli31@gmail.com>
2022-03-23 23:58:11 +0000

    create component for term and definition

---
+++ app/src/components/ContentSectionWith34.tsx
@@ -0,0 +1,49 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+import DisplayNode from './DisplayNode';
+import ContentSection from './ContentSection';
+
+interface Props {
+    xmlData?: any;
+    node?: any;
+    titleIndex?: string;
+}
+
+export default function ContentSectionWith34({
+    xmlData,
+    node,
+    titleIndex
+}: Props) {
+    const id = xmlData?.$?.id ? xmlData.$.id : '';
+    let title = xmlData.title[0];
+    title = `${titleIndex} ${title}`;
+    if (node) {
+        Object.values(node?.childNodes).map((child: any, index: number) => {
+            if (child?.tagName === 'term' || child?.tagName === 'title')
+                delete node.childNodes[index];
+        });
+        return (
+            <div className="content-section" id={id}>
+                <h1 className="title title-3">{title}</h1>
+                <DisplayNode data={node.childNodes} />
+                {xmlData?.term?.length > 0 &&
+                    xmlData.term.map((child: any, index: number) => (
+                        <ContentSection
+                            key={index}
+                            xmlData={child}
+                            titleIndex={`${titleIndex}.${index + 1}`}
+                        />
+                    ))}
+                {xmlData?.clause?.length > 0 &&
+                    xmlData.clause.map((child: any, index: number) => (
+                        <ContentSection
+                            key={index}
+                            xmlData={child}
+                            titleIndex={`${titleIndex}.${index + 1}`}
+                        />
+                    ))}
+            </div>
+        );
+    }
+
+    return <></>;
+}
