8d04857ce22a22be1b9520e654d2eb2e48156a54
Anton <anton@strogonoff.name>
2024-12-06 21:34:26 +0800

    fix: missing type stub for Lunr multi

--- packages/anafero/global.d.ts
+++ packages/anafero/global.d.ts
@@ -3,3 +3,4 @@ declare module 'lunr-languages/lunr.fr';
 declare module 'lunr-languages/lunr.ja';
 declare module 'lunr-languages/lunr.stemmer.support';
 declare module 'lunr-languages/tinyseg';
+declare module 'lunr-languages/lunr.multi';
